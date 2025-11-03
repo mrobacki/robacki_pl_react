3c0678c9d000ef27334daf9c774b48a2e834db6c
maciek <mrobacki@gmail.com>
2025-11-03 14:55:55 +0100

    scss global and path for @use

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // dzięki temu @use '@/styles/mixins' zadziała w SCSS
+        additionalData: ``,
+        // opcjonalnie ułatwia @use 'styles/...'
+        includePaths: ["src"],
+      },
+    },
+  },
+});
