547ab970c952ce0e97e802fe9ee74378e0bfc43a
maciek <mrobacki@gmail.com>
2025-11-03 15:35:22 +0100

    footer items

---
+++ src/components/Breadcrumbs.jsx
@@ -0,0 +1,25 @@
+import { Link, useLocation } from "react-router-dom";
+
+export default function Breadcrumbs() {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+  return (
+    location.pathname !== "/" && (
+      <nav aria-label="breadcrumb">
+        <Link to="/">home</Link>
+
+        {pathnames.map((name, index) => {
+          const routeTo = "/" + pathnames.slice(0, index + 1).join("/");
+          const isLast = index === pathnames.length - 1;
+
+          return (
+            <span key={name}>
+              {" / "}
+              {isLast ? <span>{name}</span> : <Link to={routeTo}>{name}</Link>}
+            </span>
+          );
+        })}
+      </nav>
+    )
+  );
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,15 @@
+import Breadcrumbs from "./Breadcrumbs";
+import Button from "./Button";
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <Breadcrumbs />
+      <span className={styles.copyright}>
+        &copy; {new Date().getFullYear()} Maciek Robacki. All rights reserved.
+      </span>
+      <Button>let’s-get-in-touch</Button>
+    </footer>
+  );
 }
